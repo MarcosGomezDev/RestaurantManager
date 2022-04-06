86cbd26f8a18e529c599ca5fbce8517297686971
Svillar87 <opi163@gmail.com>
2022-04-06 20:35:16 +0200

    sergio

--- app/src/main/java/com/example/restaurantmanager/Connect.java
+++ app/src/main/java/com/example/restaurantmanager/Connect.java
@@ -1,14 +1,50 @@
 package com.example.restaurantmanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.ref.Reference;
+
 
 public class Connect extends AppCompatActivity {
+
+    TextView tv_conexion;
+
+    private static final String PATH_START = "conexion";
+    private static final String PATH_MESSAGE = "mensaje";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect);
+
+        tv_conexion = findViewById(R.id.tv_conexion);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference reference = database.getReference(PATH_START).child(PATH_MESSAGE);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                tv_conexion.setText(snapshot.getValue(String.class));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                    Toast.makeText(Connect.this,"error al consultar",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
